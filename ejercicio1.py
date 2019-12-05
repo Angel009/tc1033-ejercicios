1cc944e43c8a8164a20b67d265a8139f5c356303
Angel <angelus_auditore@hotmail.com>
2019-12-05 00:10:12 -0600

    Implementación del Main

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 def greet(name):
     print("hello "+name)
 
@@ -6,14 +6,17 @@ def run(number):
     if number>=0 and number<=10:
         for x in range(0,number):
             print("Running")
+        print("--")
     else:
-        print(number, "no esta entre el 0 y el 10")
+        print(number, "no está entre el 0 y el 10")
 
 
 
-
-friend=str(input("Escribe tu nombre: "))
-greet(friend)
-
-num=int(input("Escriba un numero de 0 a 10: "))
-run(num)
+if __name__ == '__main__':
+    greet("Hansel")
+    greet("Grettel")
+    run(5)
+    run(8)
+    run(12)
+    run(9)
+    run(15)
